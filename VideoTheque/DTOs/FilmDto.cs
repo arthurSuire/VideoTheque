38054b587e3378ee9db33462186b7b541b424da6
JulianLeFloch <julian.lefloch1@gmail.com>
2022-12-13 11:46:28 +0100

    🎨Ajout du film DTO

---
+++ VideoTheque/DTOs/FilmDto.cs
@@ -0,0 +1,15 @@
+namespace VideoTheque.DTOs
+{
+    public class FilmDto
+    {
+        public int Id { get; set; }
+        public string IdDirector { get; set; }
+        public string IdScenarist { get; set; }
+        public int Duration { get; set; }
+        public string Support { get; set; }
+        public string IdAgeRating { get; set; }
+        public string IdGenre { get; set; }
+        public string Title { get; set; }
+        public string IdFirstActor { get; set; }
+    }
+}
