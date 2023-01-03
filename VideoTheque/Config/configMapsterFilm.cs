8bb0ed3b2b3c6c8fb746a47a5c78f5636063616b
JulianLeFloch <julian.lefloch1@gmail.com>
2023-01-03 14:52:02 +0100

    Grrr

--- VideoTheque/Config/configMapsterFilm.cs
+++ VideoTheque/Config/configMapsterFilm.cs
@@ -11,22 +11,22 @@ namespace VideoTheque.Config
             //FilmDto vers FilmViewLModel
             TypeAdapterConfig<FilmDto, FilmViewModel>
                 .NewConfig()
-                //.Map(dest => dest.Id, src => src.Id)
                 .Map(dest => dest.Director, src => src.Director.FullName)
                 .Map(dest => dest.Scenarist, src => src.Scenarist.FullName)
                 .Map(dest => dest.Duration, src => src.Duration.ToString())
-                //.Map(dest => dest.Support, src => src.Support)
                 .Map(dest => dest.AgeRating, src => src.AgeRating.Name)
                 .Map(dest => dest.Genre, src => src.Genre.Name)
-                //.Map(dest => dest.Title, src => src.Title)
                 .Map(dest => dest.FirstActor, src => src.FirstActor.FullName);
             
             //FilmViewLModel vers FilmDto
             TypeAdapterConfig<FilmViewModel, FilmDto>
                 .NewConfig()
+                .Ignore(dest => dest.Scenarist)
+                .Ignore(dest => dest.Director)
+                .Ignore(dest => dest.FirstActor)
                 .Map(dest => dest.Duration, src => src.Duration)
-                .Map(dest => dest.AgeRating.Name, src => src.AgeRating)
-                .Map(dest => dest.Genre.Name, src => src.Genre);
+                .Map(dest => dest.AgeRating, src => new AgeRatingDto{Name = src.AgeRating})
+                .Map(dest => dest.Genre, src => new GenreDto{Name = src.Genre});
         }
     }
 }
