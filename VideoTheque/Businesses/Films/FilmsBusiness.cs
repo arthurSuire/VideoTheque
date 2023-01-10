c3251aeb0d6d7ce51f51d09deeced8aca43c48e2
JulianLeFloch <julian.lefloch1@gmail.com>
2023-01-10 09:46:11 +0100

    🎨Mise à jour et essai des premiers endpoints emprunts

--- VideoTheque/Businesses/Films/FilmsBusiness.cs
+++ VideoTheque/Businesses/Films/FilmsBusiness.cs
@@ -43,17 +43,57 @@ namespace VideoTheque.Businesses.Films
             return films;
         }
 
+        public async Task<List<FilmDto>> GetFilmsPartenaire(int idPartenaire)
+        {
+            var blurays = await _bluRayDao.GetBluRays();
+            var films = new List<FilmDto>();
+            foreach (var elts in blurays)
+            {
+                if (elts.IdOwner == idPartenaire)
+                {
+                    var film = new FilmDto(elts);
+                    film.FirstActor = _personnesRepository.GetPersonne(film.FirstActor.Id).Result;
+                    film.Director = _personnesRepository.GetPersonne(film.Director.Id).Result;
+                    film.FirstActor = _personnesRepository.GetPersonne(film.FirstActor.Id).Result;
+                    film.Scenarist = _personnesRepository.GetPersonne(film.Scenarist.Id).Result;
+                    film.AgeRating = _ageRatingRepository.GetAgeRating(film.AgeRating.Id).Result;
+                    film.Genre = _genresRepository.GetGenre(film.Genre.Id).Result;
+                    film.IdOwner = elts.IdOwner;
+                    film.IsAvailable = elts.IsAvailable;
+                    films.Add(film);
+                }
+            }
+            return films;
+        }
+
         public async Task<FilmDto> GetFilm(int id)
         {
             var bluray = await _bluRayDao.GetBluRay(id);
             var film = new FilmDto(bluray);
-            Console.WriteLine("Je passe dans le bussinnes");
             film.FirstActor = _personnesRepository.GetPersonne(film.FirstActor.Id).Result;
             film.Director = _personnesRepository.GetPersonne(film.Director.Id).Result;
             film.FirstActor = _personnesRepository.GetPersonne(film.FirstActor.Id).Result;
             film.Scenarist = _personnesRepository.GetPersonne(film.Scenarist.Id).Result;
             film.AgeRating = _ageRatingRepository.GetAgeRating(film.AgeRating.Id).Result;
             film.Genre = _genresRepository.GetGenre(film.Genre.Id).Result;
+            return film;
+        }
+        
+        public async Task<FilmDto> GetFilmPartenaire(int idPartenaire, int idFilmPartenaire)
+        {
+            var bluray = await _bluRayDao.GetBluRay(idFilmPartenaire);
+            var film = new FilmDto(bluray);
+            if (bluray.IdOwner == idPartenaire)
+            {
+                film.FirstActor = _personnesRepository.GetPersonne(film.FirstActor.Id).Result;
+                film.Director = _personnesRepository.GetPersonne(film.Director.Id).Result;
+                film.FirstActor = _personnesRepository.GetPersonne(film.FirstActor.Id).Result;
+                film.Scenarist = _personnesRepository.GetPersonne(film.Scenarist.Id).Result;
+                film.AgeRating = _ageRatingRepository.GetAgeRating(film.AgeRating.Id).Result;
+                film.Genre = _genresRepository.GetGenre(film.Genre.Id).Result;
+                film.IsAvailable = bluray.IsAvailable;
+                film.IdOwner = bluray.IdOwner;
+            }
             return film;
         }
 
